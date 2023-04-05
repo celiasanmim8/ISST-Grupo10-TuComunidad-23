0e6b9ee87e3350ad0a499e2f87202d938ce6fc12
najikb <106389925+najikb@users.noreply.github.com>
2023-04-05 16:50:50 +0200

    contactos actualizado

--- frontend/src/components/Contacto/VerContacto.jsx
+++ frontend/src/components/Contacto/VerContacto.jsx
@@ -6,11 +6,32 @@ import Card from 'react-bootstrap/Card';
 const VerContacto = (props) => {
     return (
     <div class="contenedor-flexbox">
-            <Row className="my-2">
-    <Col>
-        <h2>Página de contactos</h2>
-    </Col>
-</Row>
+        <Row className="my-2">
+            <Col>
+                <Card className="flex-fill text-wrap">
+                    <Card.Body>
+                        <Card.Title href="#">Presidente</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                    <p>Contactos del presidente</p>
+                            </Card.Subtitle>
+                        <Card.Text className="text-truncate"><b>Teléfono:</b> 000000000</Card.Text>
+                        <Card.Text className="text-truncate"><b>Email:</b> presidente@correo.com</Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col>
+                <Card className="flex-fill text-wrap">
+                    <Card.Body>
+                        <Card.Title href="#">Administrador</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                    <p>Contactos del administrador</p>
+                            </Card.Subtitle>
+                        <Card.Text className="text-truncate"><b>Teléfono:</b> 000000000</Card.Text>
+                        <Card.Text className="text-truncate"><b>Email:</b> admin@correo.com</Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
     </div>
 
     );
