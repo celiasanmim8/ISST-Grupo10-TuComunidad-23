d78cd0bb1b2b11f3f9ea0df9b0a51e85069faa48
javier-ariza <80358434+javier-ariza@users.noreply.github.com>
2023-04-10 23:31:49 +0200

    Mejora estilo componente VerComentario
    Se quitan los espacios que aparecían entre el botón Responder y las respuestas

--- frontend/src/components/Comentarios/VerComentario.jsx
+++ frontend/src/components/Comentarios/VerComentario.jsx
@@ -7,13 +7,14 @@ import Card from 'react-bootstrap/Card';
 const VerComentario = (props) => {
     const comentariosList = props.comentariolist;
     const id = props.sugerenciaId;
-    console.log(id);
     return (
         <div class="contenedor-flexbox">
             <Container>
                 {comentariosList.map((comentarioItem) => (
+                    <div>
+                         {(comentarioItem.sugerenciaId === id ? 
                     <Row className="my-2">
-                        {(comentarioItem.sugerenciaId === id ? 
+                       
                             <Col key={comentarioItem.id}>
                                 <Card className="flex-fill text-wrap">
                                     <Card.Body>
@@ -21,8 +22,10 @@ const VerComentario = (props) => {
                                     </Card.Body>
                                 </Card>
                             </Col>
-                        : null )}    
+                 
                     </Row>
+                           : null )}    
+                    </div>
                 ))}
             </Container>
         </div>
