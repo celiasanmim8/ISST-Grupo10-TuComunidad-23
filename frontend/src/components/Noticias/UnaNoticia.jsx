7d2842fb29f17fe5afa94bfbe98302b1c6586c7a
VMindaros <vinlorenzmindaros@gmail.com>
2023-04-12 18:27:12 +0200

    Añadida visualización de imagen en noticia individual con opción de expandir/comprimir

--- frontend/src/components/Noticias/UnaNoticia.jsx
+++ frontend/src/components/Noticias/UnaNoticia.jsx
@@ -1,24 +1,51 @@
-import { Button, Col, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Col, Image, Row, Container } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 
 
 const UnaNoticia = (props) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleImageSize = () => {
+        setIsExpanded(!isExpanded);
+    }
+
+    const smallSize = { maxWidth: '10em', cursor: 'pointer' };
+    const largeSize = { maxWidth: '100%', cursor: 'pointer' };
+    const imageSize = isExpanded ? largeSize : smallSize;
+
     let { noticiaId } = useParams();
     let position = noticiaId;
     const noticiaslist = props.noticiaslist;
 
     return (
-        <Row className="mx-0 my-0 w-100">
-            <Col>
-                <p className="news-title"><b>{noticiaslist[position - 1].titulo}</b></p>
-                <p className="news-description">{noticiaslist[position - 1].descripcion}</p>
+        <Container style={{ maxHeight: '100vh', overflowY: 'scroll' }}>
+            <Row className="text-break">
+                <Col>
+                    <p className="news-title"><b>{noticiaslist[position - 1].titulo}</b></p>
+                    <p className="news-description">{noticiaslist[position - 1].descripcion}</p>
+                </Col>
+            </Row>
+            <Row className="justify-content-center my-3 mx-5">
+                {noticiaslist[position - 1].adjunto && (
+                    <Image
+                        src={`data:image/*;base64,${noticiaslist[position - 1].adjunto}`}
+                        rounded="true"
+                        thumbnail="true"
+                        className="justify-center"
+                        style={imageSize}
+                        onClick={toggleImageSize}
+                    />
+                )}
+            </Row>
+            <Row className="mb-3">
                 <div className="news-button">
                     <Link to={'/noticias'}>
                         <Button>Volver</Button>
                     </Link>
                 </div>
-            </Col>
-        </Row>
+            </Row>
+        </Container>
     );
 };
 
