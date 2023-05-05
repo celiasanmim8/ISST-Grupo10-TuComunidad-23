943989e31da97a030468909dc1dee24654f00353
najikb <106389925+najikb@users.noreply.github.com>
2023-05-05 11:10:33 +0200

    Contador global de juntas

--- frontend/src/components/Votaciones/VerVoto.jsx
+++ frontend/src/components/Votaciones/VerVoto.jsx
@@ -1,15 +1,17 @@
 import { Col, Row } from "react-bootstrap";
 import { Container } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
 
 import Card from 'react-bootstrap/Card';
 
 import useRequireAuth from '../Login/useRequireAuth';
+import { useState } from "react";
 
 
 const VerVoto = (props) => {
     useRequireAuth();
     const votoslist = props.votoslist;
-    const id = props.juntaId;
+    const juntaId = props.juntaId;
     
     const contarVotosPorJunta = (votoslist, juntaId) => {
         let votosAFavor = 0;
@@ -45,31 +47,67 @@ const VerVoto = (props) => {
           votosAbstencion,
           opcionGanadora
         };
-      };
-    const votosPorJunta = contarVotosPorJunta(votoslist, id);
+    };
+
+    const [showVotes, setShowVotes] = useState(null);
+
+    const handleShowVotes = (votoId) => {
+        setShowVotes(votoId);
+    }
+
+    const handleToggleVotes = () => {
+        if (showVotes === null) {
+            setShowVotes(1);
+        } else {
+            setShowVotes(null);
+        }
+    }
+
     return (
         <div className="contenedor-flexbox">
             <Container>
                 {votoslist.map((VotoItem) => (
                     <div key={VotoItem.id}>
-                        {(VotoItem.juntaId === parseInt(id) ?
+                        {(VotoItem.juntaId === juntaId ?
                             <Row className="my-2">
                                 <Col>
-                                    <Card className="flex-fill text-wrap">
+                                    <Card className="flex-fill text-wrap bg-dark">
                                         <Card.Body >
-                                            <Card.Subtitle className="text-muted">Usuario XYZ votó:</Card.Subtitle>
-                                            <Card.Text className="text-truncate">{VotoItem.voto}</Card.Text>
-                                            <Card.Subtitle className="text-muted">{`${votosPorJunta.votosAFavor} votos a favor, ${votosPorJunta.votosEnContra} votos en contra, ${votosPorJunta.votosAbstencion} abstenciones. Opción ganadora: ${votosPorJunta.opcionGanadora}`}</Card.Subtitle>
+                                            <Card.Subtitle className="text-muted text-white">Usuario {VotoItem.userId} votó:</Card.Subtitle>
+                                            <Card.Text className="text-truncate text-white"style={{fontWeight: 700}}>{VotoItem.voto}</Card.Text>
                                         </Card.Body>
-                                    </Card>
+                                        </Card>
                                 </Col>
                             </Row>
-                            : null)}
+                            : null)
+                        }
                     </div>
                 ))}
+                <Row className="my-2">
+                    <Col>
+                        <Button onClick={handleToggleVotes}>
+                            {showVotes === null ? 'Mostrar votos de todas las opciones' : 'Ocultar votos'}
+                        </Button>
+                    </Col>
+                </Row>
+                {showVotes !== null && (
+                    <Row className="my-2">
+                        <Col>
+                            <Card>
+                                <Card.Body>
+                                <Card.Title>
+                                  Votos totales para esta junta:
+                                  <span style={{ color: 'green', fontWeight: 700}}>{` ${contarVotosPorJunta(votoslist, juntaId).votosAFavor} votos a favor, `}</span>
+                                  <span style={{ color: 'red', fontWeight: 700 }}>{`${contarVotosPorJunta(votoslist, juntaId).votosEnContra} votos en contra, `}</span>
+                                  <span style={{ color: 'orange', fontWeight: 700 }}>{`${contarVotosPorJunta(votoslist, juntaId).votosAbstencion} abstenciones`}</span>
+                                </Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                )}
             </Container>
         </div>
     );
 };
-
 export default VerVoto;
